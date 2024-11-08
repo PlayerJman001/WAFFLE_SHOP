053f9855d67c6935c251b1c14f6f2a2cfb5c43bf
PlayerJman001 <93268562+PlayerJman001@users.noreply.github.com>
2024-11-08 12:01:33 +0100

    js is the end off me
    changed a couple of stuff, mainly changed a lot of classes to id cause js likes id more than classes

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-const soldEl = document.getElementsByClassName("sold")
-const storedEl = document.getElementsByClassName("storage")
-const profitEl = document.getElementsByClassName("money")
+const soldEl = document.getElementById("sold");
+const storedEl = document.getElementById("storage");
+const profitEl = document.getElementById("money");
+const makebtn = document.getElementById("make");
+const sellbtn = document.getElementById("sell");
+
+let sold = 0;
+let stored = 0;
+let money = 0;
+
+update()
+
+function update()
+    {
+
+        storedEl.innerText = stored;
+        profitEl.innerText = money + "$";
+        soldEl.innerText = sold;
+    };
+
+makebtn.addEventListener("click", () => {
+
+    stored = stored + 1;
+    update()
+
+});
+
+sellbtn.addEventListener("click", () => {
+
+    sold = sold + stored;
+    money = stored * 20 + money;
+    stored = stored - stored;
+    update()
+
+});
